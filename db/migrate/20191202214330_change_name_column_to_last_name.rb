a8734232383c774442790da28ceccfdd3f19a918
averagealloy <m.mctighe10@gmail.com>
2019-12-02 16:53:10 -0500

    fixin devise

---
+++ db/migrate/20191202214330_change_name_column_to_last_name.rb
@@ -0,0 +1,5 @@
+class ChangeNameColumnToLastName < ActiveRecord::Migration[6.0]
+  def change
+    rename_column :users, :name, :last_name 
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_02_192738) do
+ActiveRecord::Schema.define(version: 2019_12_02_214330) do
 
   create_table "cars", force: :cascade do |t|
     t.integer "owner_id"
@@ -44,8 +44,9 @@ ActiveRecord::Schema.define(version: 2019_12_02_192738) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "name"
+    t.string "last_name"
     t.string "address"
+    t.string "first_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
