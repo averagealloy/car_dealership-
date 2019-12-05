9da80bd27f23106db9d957201bbc2c3f30279b85
averagealloy <m.mctighe10@gmail.com>
2019-12-05 13:28:01 -0500

    displaying the the car assoicated with a dealership

--- app/controllers/dealerships_controller.rb
+++ app/controllers/dealerships_controller.rb
@@ -3,4 +3,8 @@ class DealershipsController < ApplicationController
     def index 
         @dealerships = Dealership.all
     end 
+
+    def show
+         @dealerships = Dealership.find(params[:id])
+    end 
 end
