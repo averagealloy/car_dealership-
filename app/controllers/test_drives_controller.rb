fd3a802c5092fa0d7febdb77fd39910d00b03141
averagealloy <m.mctighe10@gmail.com>
2019-12-07 17:00:40 -0500

    working on connecting my testdrive good bones but need to work on it

--- app/controllers/test_drives_controller.rb
+++ app/controllers/test_drives_controller.rb
@@ -1,16 +1,27 @@
 class TestDrivesController < ApplicationController
 
    def new 
-    @car = Car.find(params[:car_id])
-    @test_drive = @car.test_drives.build 
+      @car = Car.find(params[:car_id])
+      @test_drive = @car.test_drives.build 
     end 
 
 
    def create
-      @test_drive = current_user.test_drives.build(test_drive_params)
-      #probly wrong 
+      # @test_drive = current_user.test_drives.build(car_id: params[:car_id], 
+      #    test_drive_date: params[:test_drifes_params])
+      #    if @test_drive.save
+      #       redirect_to 
+      #    else 
+      #       render :new
+      #    end 
+      render :show
    end  
 
+   private
+
+   def test_drifes_params
+       params.require(:test_drife).permit("test_drive_date(1i)", "test_drive_date(2i)", "test_drive_date(3i)", "car_id") 
+   end 
 
 end
 
