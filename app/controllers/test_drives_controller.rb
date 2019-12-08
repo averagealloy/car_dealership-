3b7b1f9b6096d3ff5baa2bf24e7ed7221f3a861a
averagealloy <m.mctighe10@gmail.com>
2019-12-08 16:36:17 -0500

    now when creating a test drive is not saving but is going to a tester page

--- app/controllers/test_drives_controller.rb
+++ app/controllers/test_drives_controller.rb
@@ -8,14 +8,14 @@ class TestDrivesController < ApplicationController
 
 
    def create
-      
-      @test_drive = current_user.test_drives.build(car_id: params[:car_id], 
+       @test_drive = current_user.test_drives.build(car_id: params[:car_id], 
          test_drive_date: params[:test_drifes_params])
          if @test_drive.save
-            redirect_to  car_test_drife_path(@test_drive.car)
+            redirect_to car_test_drives_path(@test_drive.car)
          else 
-            render :new
-         end 
+            render :index
+         end
+
      
    end  
 
@@ -23,11 +23,17 @@ class TestDrivesController < ApplicationController
       @test_drive = TestDrife.find_by(id: params[:id])
    end 
 
+   def index 
+   end 
+
    private
 
    def test_drifes_params
        params.require(:test_drife).permit("test_drive_date(1i)", "test_drive_date(2i)", "test_drive_date(3i)", "car_id") 
    end 
+
+      
+
 
 end
 
