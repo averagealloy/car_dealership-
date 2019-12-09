6bb601273bc53e85fd87dba92303c75b73e40b05
averagealloy <m.mctighe10@gmail.com>
2019-12-09 13:02:11 -0500

    getting the datetime and the name of the car interpolationg

--- app/models/test_drife.rb
+++ app/models/test_drife.rb
@@ -1,23 +1,7 @@
 class TestDrife < ApplicationRecord
   belongs_to :user
   belongs_to :car
-  #whats going on here look dow ask someone if this check out 
   has_many :dealerships, through: :cars 
 
-  # validates :test_drive_date, presence: true
-  
-  
-  # accepts_nested_attributes_for :car
-
-  
-  # def test_drive_date= (hash = {})
-  #   self.test_drive_date = "#{'test_drive_date(1i)'} , #{'test_drive_date(2i)'} , #{
-  #   'test_drive_date(3i)'}"  
-  # end 
-
-  # def test_drive_date=(attributes = {})
-  #   if attributes[:date].present?
-  #     self.car = Car.find_or_create_by(attributes)
-  #   end 
-  # end 
+  validates :test_drive_date, presence: true
 end
