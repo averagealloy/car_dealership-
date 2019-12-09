eed3810d34e095968d6194faad72b2224e800f07
averagealloy <m.mctighe10@gmail.com>
2019-12-08 21:03:47 -0500

    create method is still shot

--- app/controllers/test_drives_controller.rb
+++ app/controllers/test_drives_controller.rb
@@ -9,8 +9,9 @@ class TestDrivesController < ApplicationController
 
    def create
        
-       @test_drive = current_user.test_drives.build(car_id: params[:car_id], test_drive_date: test_drifes_params)
-         byebug
+      #  @test_drive = current_user.test_drives.build(car_id: params[:car_id], test_drive_date: test_drifes_params)
+      @test_drive = current_user.test_drives.build(test_drifes_params)
+         
          if @test_drive.save
             redirect_to car_test_drife_path(@test_drive.car)
          else 
@@ -28,11 +29,10 @@ class TestDrivesController < ApplicationController
    private
 
    def test_drifes_params
-       params.require(:test_drife).permit("test_drive_date(1i)", "test_drive_date(2i)", "test_drive_date(3i)", "car_id") 
-   end 
-
       
+         params.require(:test_drife).permit(:date, :car_id, :user_id, car_attributes: [:date])
 
+   end 
 
 end
 
--- app/models/test_drife.rb
+++ app/models/test_drife.rb
@@ -5,8 +5,19 @@ class TestDrife < ApplicationRecord
   has_many :dealerships, through: :cars 
 
   validates :test_drive_date, presence: true
-  def test_drive_date= (hash = {})
-    self.test_drive_date = "#{'test_drive_date(1i)'} , #{'test_drive_date(2i)'} , #{
-    'test_drive_date(3i)'}"  
-  end 
+  
+  
+  accepts_nested_attributes_for :car
+
+  
+  # def test_drive_date= (hash = {})
+  #   self.test_drive_date = "#{'test_drive_date(1i)'} , #{'test_drive_date(2i)'} , #{
+  #   'test_drive_date(3i)'}"  
+  # end 
+
+  # def test_drive_date=(attributes = {})
+  #   if attributes[:date].present?
+  #     self.car = Car.find_or_create_by(attributes)
+  #   end 
+  # end 
 end
